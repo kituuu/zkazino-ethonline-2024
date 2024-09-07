56f332da86caadb6af987a5a9e20b7c7e3051e7e
Karthik Ayangar <karthik.ayangar7118@gmail.com>
2024-09-07 15:02:47 +0530

    feat: state and amt fix.

--- apps/web/components/pages/Poker/game/Game.tsx
+++ apps/web/components/pages/Poker/game/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import Cards from './Cards';
 
 import { Deck, getCards } from '@/games/pokerShowdown/utils/deck';
@@ -11,14 +11,19 @@ import {
   getWinner,
   Hand,
 } from '@/games/pokerShowdown/utils/gameFunctions';
+import { useNetworkStore } from '@/lib/stores/network';
+import { useRandzuMatchQueueStore } from '@/games/pokerShowdown/stores/matchQueue';
+import { GameState } from '@/games/pokerShowdown/lib/gameState';
 
 // let socket;
 // const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
-
+const buttonBaseStyle =
+  'font-bold py-2 px-4 rounded-lg transition-colors duration-200';
+const buttonEnabledStyle = 'hover:brightness-110 active:brightness-90';
+const buttonDisabledStyle = 'disabled:opacity-50 disabled:cursor-not-allowed';
 interface IGameViewProps {
   gameInfo: IGameInfo<PokerCards> | undefined;
-  matchInfo: MatchQueueState;
-  superIncrement: (from: PublicKey, amount?: number) => Promise<void>;
+  setGameState: Dispatch<SetStateAction<GameState>>;
   handleCall: () => void;
   handleRaise: (amount: number) => void;
   handleFold: () => void;
@@ -26,16 +31,24 @@ interface IGameViewProps {
   // loading: boolean;
 }
 
-const Game = ({ gameInfo, matchInfo, superIncrement, handleCall, handleRaise, handleFold }: IGameViewProps) => {
+const Game = ({
+  gameInfo,
+  setGameState,
+  handleCall,
+  handleRaise,
+  handleFold,
+}: IGameViewProps) => {
   const currentUser = gameInfo?.currentUserIndex == 0 ? 'Player 1' : 'Player 2';
   // const currentUser = gameInfo?.currentMoveUser.equals(gameInfo?.player1) ? 'Player 1' : 'Player 2';
-
-
+  const { address } = useNetworkStore();
 
   // Initialize game state
+
   const [gameOver, setGameOver] = useState<boolean | undefined>(false);
   const [turn, setTurn] = useState('Player 1');
-  const [numberOfTurns, setNumberOfTurns] = useState(0);
+  const [numberOfTurns, setNumberOfTurns] = useState<number>(
+    Number(gameInfo?.field.numberOfTurns.value.value[1][1])
+  );
   const player1Deck: Deck[] = getCards(
     gameInfo?.field.player1Cards as Card[]
   ) as Deck[];
@@ -46,7 +59,7 @@ const Game = ({ gameInfo, matchInfo, superIncrement, handleCall, handleRaise, ha
     gameInfo?.field.houseCards as Card[]
   ) as Deck[];
   const [houseDeck, setHouseDeck] = useState<Deck[]>(saarepatte.slice(0, 3));
-  const [winner, setWinner] = useState('');
+  const [winner, setWinner] = useState<string>('');
   const [player1Chips, setPlayer1Chips] = useState(0);
   const [player2Chips, setPlayer2Chips] = useState(0);
   const [increment, setIncrement] = useState(10);
@@ -58,8 +71,8 @@ const Game = ({ gameInfo, matchInfo, superIncrement, handleCall, handleRaise, ha
   const keyToPlayer = {
     [gameInfo?.player1?.toBase58() as string]: 'Player 1',
     [gameInfo?.player2?.toBase58() as string]: 'Player 2',
-  }
-
+  };
+  const matchQueue = useRandzuMatchQueueStore();
   const [localHand, setLocalHand] = useState('N/A');
 
   const callHandler = async () => {
@@ -69,7 +82,7 @@ const Game = ({ gameInfo, matchInfo, superIncrement, handleCall, handleRaise, ha
   const raiseHandler = async (amount: number) => {
     // TODO: add logic for raise transaction
     // await superIncrement(gameInfo?.currentMoveUser as PublicKey, amount);
-    await handleRaise(amount*1000000000);
+    await handleRaise(amount * 1000000000);
   };
 
   const foldHandler = async () => {
@@ -79,25 +92,26 @@ const Game = ({ gameInfo, matchInfo, superIncrement, handleCall, handleRaise, ha
   const [restart, setRestart] = useState(false);
 
   useEffect(() => {
-    setIncrement((gameInfo?.field.increment.value.toString() as number)/1000000000);
-    setPlayer1Chips((gameInfo?.field.player1Chips.value.toString() as number)/1000000000)
-    setPlayer2Chips((gameInfo?.field.player2Chips.value.toString() as number)/1000000000);
+    setIncrement(
+      (gameInfo?.field.increment.value.toString() as number) / 1000000000
+    );
+    setPlayer1Chips(
+      (gameInfo?.field.player1Chips.value.toString() as number) / 1000000000
+    );
+    setPlayer2Chips(
+      (gameInfo?.field.player2Chips.value.toString() as number) / 1000000000
+    );
     setPot((gameInfo?.field.pot.value.toString() as number) / 1000000000);
-    setRaiseAmount(gameInfo?.field.player1Bet.value.toString()); 
+    setRaiseAmount(gameInfo?.field.player1Bet.value.toString());
     setPlayer1Name(
-      //matchInfo?.player1Name 
+      //matchInfo?.player1Name
       keyToPlayer[gameInfo?.player1?.toBase58() as string] || 'Player 1'
     );
 
     setPlayer2Name(
-      //matchInfo?.player2Name 
-       keyToPlayer[gameInfo?.player2?.toBase58() as string] || 'Player 2'
+      //matchInfo?.player2Name
+      keyToPlayer[gameInfo?.player2?.toBase58() as string] || 'Player 2'
     );
-    console.log('deep', player1Chips);
-    console.log('azwesrdctfvyguinmoxdrcftvugybj',gameInfo?.field)
-    console.log("alskdjfalskdfjasldkfjLKJLDKJFLSKDJFLSDKJF>>>>>>>>>>>player1", gameInfo?.player1.toBase58())
-    console.log("alskdjfalskdfjasldkfjLKJLDKJFLSKDJFLSDKJF>>>>>>>>>>>moveuser", gameInfo?.currentMoveUser.toBase58())
-    
 
     setTurn(
       gameInfo?.currentMoveUser.equals(gameInfo?.player1).toBoolean()
@@ -106,10 +120,8 @@ const Game = ({ gameInfo, matchInfo, superIncrement, handleCall, handleRaise, ha
     );
   }, [gameInfo]);
 
-
-  
-
   useEffect(() => {
+    setNumberOfTurns(Number(gameInfo?.field.numberOfTurns.value.value[1][1]));
     // if (numberOfTurns === 0) {
     //   setGameOver(true);
     //   setWinner(
@@ -121,31 +133,40 @@ const Game = ({ gameInfo, matchInfo, superIncrement, handleCall, handleRaise, ha
     //     )
     //   );
     // }
+    console.log(Number(numberOfTurns));
+    console.log(numberOfTurns === 4);
     if (numberOfTurns === 2) {
       setIncrement(gameInfo?.field.increment.value.toString() as number);
-      // socket.emit('updateGameState', { increment: 0 });
-      // playShufflingSound();
     } else if (numberOfTurns === 4) {
-      // socket.emit('updateGameState', {
       setHouseDeck(saarepatte.slice(0, 4));
-
       setIncrement(gameInfo?.field.increment.value.toString() as number);
     } else if (numberOfTurns === 6) {
-      console.log(
-        'madfhajk sdjkfhjkahsjdhf jkdhsjahsdkjf hjkashdfjk hajksdh jkfahskj'
-      );
       setHouseDeck(saarepatte);
       setIncrement(gameInfo?.field.increment.value.toString() as number);
     } else if (numberOfTurns === 8) {
       setGameOver(true);
-      setWinner(
-        getWinner(
-          'Player 1',
-          'Player 2',
-          getHand(player1Deck, houseDeck) as Hand,
-          getHand(player2Deck, houseDeck) as Hand
-        )
+      const winner = getWinner(
+        'Player 1',
+        'Player 2',
+        getHand(player1Deck, houseDeck) as Hand,
+        getHand(player2Deck, houseDeck) as Hand
       );
+      setWinner(winner);
+      console.log(winner);
+      console.log(currentUser);
+      if (winner == 'Player 2') {
+        if (address == gameInfo?.player2.toBase58()) {
+          matchQueue.setLastGameState('win');
+        } else {
+          matchQueue.setLastGameState('lost');
+        }
+      } else if (winner == 'Player 1') {
+        if (address == gameInfo?.player1.toBase58()) {
+          matchQueue.setLastGameState('win');
+        } else {
+          matchQueue.setLastGameState('lost');
+        }
+      }
     }
 
     if (!gameOver && currentUser === 'Player 1')
@@ -153,23 +174,6 @@ const Game = ({ gameInfo, matchInfo, superIncrement, handleCall, handleRaise, ha
     else if (!gameOver && currentUser === 'Player 2')
       setLocalHand(getHand(player2Deck, houseDeck) as string);
   }, [gameInfo?.field.numberOfTurns]);
-  console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<saarepatte')
-  console.log(gameInfo?.field);
-  console.log("turn",turn)
-  console.log("bool", gameInfo?.currentMoveUser.equals(gameInfo?.player1).toBoolean)
-  const a = {
-    currentUser,
-    turn,
-    player1Chips,
-    player2Chips,
-    increment,
-    gameOver,
-  }
-  console.log("???>>>>>?", a)
-  console.log("HALLL>>>>>>>", (currentUser !== turn ||
-    (currentUser === 'Player 2' && player2Chips < increment) ||
-    (currentUser === 'Player 1' && player1Chips < increment) ||
-    gameOver))
   return (
     <div className="game-bg noselect">
       <div className="game-board">
@@ -195,24 +199,24 @@ const Game = ({ gameInfo, matchInfo, superIncrement, handleCall, handleRaise, ha
           {!gameOver && (
             <>
               <button
-                disabled={
-                  currentUser !== turn ||
-                  (currentUser === 'Player 2' && player2Chips < increment) ||
-                  (currentUser === 'Player 1' && player1Chips < increment) ||
-                  gameOver
-                }
+                className={`${buttonBaseStyle} ${buttonDisabledStyle} ${buttonEnabledStyle} bg-purple-500 text-white`}
+                disabled={gameInfo?.currentMoveUser.toBase58() != address}
                 onClick={() => callHandler()}
               >
-                {(raiseAmount === 0 &&
+                {(raiseAmount / 1000000000 === 0 &&
                   increment &&
                   numberOfTurns < 2 &&
-                  `Buy In(${increment})`) ||
-                  (raiseAmount === 0 && increment && `Call(${increment})`) ||
-                  (raiseAmount > 0 && `Call(${raiseAmount})`) ||
+                  `Buy In(${increment / 1000000000})`) ||
+                  (raiseAmount === 0 &&
+                    increment &&
+                    `Call(${increment / 1000000000})`) ||
+                  (raiseAmount > 0 && `Call(${raiseAmount / 1000000000})`) ||
                   'Check'}
               </button>
               <RaiseModal
-                minRaise={raiseAmount > 0 ? raiseAmount : increment}
+                minRaise={
+                  (raiseAmount > 0 ? raiseAmount : increment) / 1000000000
+                }
                 maxRaise={
                   currentUser === 'Player 1' ? player1Chips : player2Chips
                 }
@@ -251,7 +255,3 @@ const Game = ({ gameInfo, matchInfo, superIncrement, handleCall, handleRaise, ha
 };
 
 export default Game;
-
-
-
-
